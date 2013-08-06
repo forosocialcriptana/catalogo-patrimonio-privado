81b29248ebefcfb050ebb1d5fc385012ec4f0935
Alfredo Sánchez Alberca <asalber@gmail.es>
2013-08-06 13:06:52 +0200

    Primera version

---
+++ javascripts/form.js
@@ -0,0 +1,26 @@
+$(function(){
+    //Add href and disable some fields
+    $('form').attr('action','https://docs.google.com/forms/d/19EuPG4RaFQyDledhbTa7zx4jDEiUpsZuWrhgJV-LKGk/formResponse');
+});
+
+
+function Valida(formulario){
+	if ($("#entry_1016167564").val().length==0) {
+        alert("Introduzca el título del lugar");
+        return false;
+    } else if ($("#entry_145325308").val().length==0 ) {
+        alert("Introduzca la descripción breve del lugar");
+        return false;
+    } else if ($("#entry_1350527290").val().length==0 ) {
+        alert("Introduzca la latitud del lugar");
+        return false;
+    } else if ($("#entry_1394268219").val().length==0 ) {
+        alert("Introduzca la longitud del lugar");
+        return false;
+     } else if ($("entry_145325308").val().length>800 ) {
+        alert("El número máximo de caracteres para la descripción breve es de 800. Puedes utilizar la descripción completa para escribir más.");
+        return false;
+    } else {
+        return true;
+    }
+}
